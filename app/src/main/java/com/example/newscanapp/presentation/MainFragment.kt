a293682651bac6c57fcc35b333997b2a7a834129
DmitrySergienko <dmitrisergienko@gmail.com>
2023-03-09 09:41:59 +0300

    compose dependency attached

---
+++ app/src/main/java/com/example/newscanapp/presentation/MainFragment.kt
@@ -0,0 +1,18 @@
+package com.example.newscanapp.presentation
+
+import android.os.Bundle
+import com.example.newscanapp.databinding.FragmentMainBinding
+import com.example.newscanapp.presentation.utils.BaseFragment
+
+
+class MainFragment:
+    BaseFragment<FragmentMainBinding>(FragmentMainBinding::inflate) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/newscanapp/presentation/utils/BaseFragment.kt
@@ -0,0 +1,32 @@
+package com.example.newscanapp.presentation.utils
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<T : ViewBinding>(
+    private val inflate: (inflater: LayoutInflater, parent: ViewGroup?, attachToRoot: Boolean) -> T
+) : Fragment() {
+
+    private var _binding: T? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = inflate.invoke(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
